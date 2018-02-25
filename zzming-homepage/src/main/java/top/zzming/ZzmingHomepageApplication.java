385f0b86f4ff912e3bddd8457f1634c18dceafb7
ZhuZhanming <1020030139@qq.com>
2018-02-25 14:09:34 +0800

    增加登录之后的动态变化

--- zzming-homepage/src/main/java/top/zzming/ZzmingHomepageApplication.java
+++ zzming-homepage/src/main/java/top/zzming/ZzmingHomepageApplication.java
@@ -3,11 +3,7 @@ package top.zzming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableFeignClients
